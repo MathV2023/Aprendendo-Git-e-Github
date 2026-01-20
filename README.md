51e06caa50e34e1b3bda826ac4daa6d3756571ab
MathV2023 <matheusviniciusdias2024@gmail.com>
2026-01-20 08:12:36 -0300

    Melhoria no jogo de terminal
    Alterado o 'main.lua' do jogo para informar se o número dado pela pessoa é maior ou menor que o número aleatorio gerado, além do jogo estar funcional, sendo também adicionado um 'README.md' descritivo simples do jogo.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jogo: Adivinhe o número
+
+**Obs.:** Este é um jogo criado em lua para eu aprender como usar o git.
+
+## Mecânicas:
+- O jogo irá escolher um número aleátorio que é limitado a ser 1, 100 ou outro número natural entre eles;
+- É dada 7 tentativas para acertar o número, se acabar as todas as suas chances você perdeu, já se tu tiver acertado o número gerado pelo jogo, você vence o ele.
--- main.lua
+++ main.lua
@@ -2,34 +2,62 @@
 -- Modulos importantes
 local math = require("math")
 local io = require("io")
+local os = require("os")
+
+
+-- Tratamento de versão
+local minor = tonumber(_VERSION:match("%d%.(%d)"))
+
+if (minor < 4) then
+   math.randomseed(os.time())
+end
 
 
 -- Variaveis Locais
 local numero_aleatorio = math.random(1, 100)
 local numero_chutado = 0
-local numero_chaces = 7
-local is_perdeu = false
+local chances = 7
 
 
-print("Jogo: Adivinhe o numero\n\t Voce tem " .. numero_chaces .. " chances de acerto.")
-io.write("Qual e o numero na lista de 1 a 100: ");
-numero_chutado = io.read("*n")
-
-while (numero_chutado ~= numero_aleatorio) do
-   numero_chaces = numero_chaces - 1
-   print("Errou")
-   print("Voce tem " .. numero_chaces .. " chances de acerto.")
-   io.write("Qual e o numero na lista de 1 a 100: ");
-   numero_chutado = io.read("*n")
-
-   if (numero_chaces == 0) then
-      is_perdeu = true
-      break
+-- Função local
+local function readNumber()
+   local valor = tonumber(io.read()) or ""
+   if (type(valor) == "number") then
+      return valor
    end
 end
 
-if (is_perdeu) then
-   print("Game Over: \n\tO numero era " .. numero_aleatorio)
-elseif (numero_chutado == numero_aleatorio) then
-   print("Vitoria: \n\tO numero era " .. numero_aleatorio)
+
+print("Jogo: Adivinhe o número")
+
+while (numero_chutado ~= numero_aleatorio) do
+   if (chances == 0) then
+      print("Game Over: \n\tO número era " .. numero_aleatorio)
+      os.exit()
+   else
+      if (chances == 1) then
+         print("\tVocê tem uma chance.")
+      else
+         print("\tVocê tem " .. chances .. " chances.")
+      end
+
+      io.write("Qual é o número na lista de 1 a 100: ")
+      numero_chutado = readNumber()
+      print(tostring(numero_chutado))
+      
+      while (type(numero_chutado) ~= "number") do
+         io.write("Coloque um número na lista de 1 a 100: ")
+         numero_chutado = readNumber()
+      end
+
+      if (numero_chutado > numero_aleatorio) then
+         print("O número " .. numero_chutado .. " é 'maior' que o número que eu guardo.")
+      elseif (numero_chutado < numero_aleatorio) then
+         print("O número " .. numero_chutado .. " é 'menor' que o número que eu guardo.")
+      end
+
+      chances = chances - 1
+   end
 end
+
+print("Acertou: \n\tO número era " .. numero_aleatorio)
